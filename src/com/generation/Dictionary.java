44c28de5fd1a30a67affdeed5701a1805bc1a15c
BryanTevillo <tevillo24@gmail.com>
2024-02-23 13:51:37 -0600

    Se termina ejercicio con buenas practias y comentarios

--- src/com/generation/Dictionary.java
+++ src/com/generation/Dictionary.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Dictionary {
 
   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    // Se declara HashMap para almacenar el diccionario
     HashMap<String, String> dictionary = new HashMap<>();
+
+    // Se guardan pares clave/valor
     dictionary.put("Correr", "Run");
     dictionary.put("Hablar", "Talk");
     dictionary.put("Computadora", "Computer");
@@ -23,8 +28,8 @@ public class Dictionary {
     dictionary.put("Ventana", "Window");
 
     dictionary.put("Mano", "Hand");
-    dictionary.put("Head", "Cabeza");
-    dictionary.put("Popcorn", "Palomitas");
+    dictionary.put("Cabeza", "Head");
+    dictionary.put("Palomitas", "Popcorn");
     dictionary.put("Impresora", "Printer");
     dictionary.put("Carro", "Car");
     dictionary.put("Silla", "Chair");
@@ -34,10 +39,27 @@ public class Dictionary {
     dictionary.put("Tenedor", "Fork");
     dictionary.put("Tio", "Uncle");
 
-    Set<String> setKey = dictionary.keySet();
-    List<String> listKey = new ArrayList<>(setKey);
-    for (int i = 4; i >= 0; i--) {
-      System.out.println(listKey.get(new Random().nextInt(20)));
+    // Se realiza una ArrayList del conjunto de claves del diccionario
+    List<String> listKey = new ArrayList<>(dictionary.keySet());
+    int count = 0;
+    int numberQuestions = 5;
+
+    // Se realiza un for para las pregntas 5
+    for (int i = numberQuestions; i >= 1; i--) {
+      // se obtiene una key random usando metodo nextInt de la clase Random
+      String spanishWord = listKey.get(new Random().nextInt(20));
+      System.out.println("Ingresa la traduccion del ingles de la siguiente palabra: " + spanishWord);
+      String englishWord = in.nextLine();
+      // Se valida cuantas respues buenas obtuvo el cliente
+      if (englishWord.toUpperCase().equals(dictionary.get(spanishWord).toUpperCase())) {
+        count++;
+      }
     }
+
+    // Se imprime el resultado
+    System.out.println("Respuestas correctas: " + count + " | Respuestas incorrectas: " + (count - numberQuestions)
+        + " | " + "De un total de preguntas: " + numberQuestions);
+
+    in.close();
   }
 }
